1d7c174b8357be6683652669f90595008faf7df1
KaranBamaniya-BTC <karan.bamaniya@botreetechnologies.com>
2016-09-29 18:10:00 +0530

    other changes

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -8,22 +8,22 @@ showDatePicker: function(){
 partialHideShow: function(){
   $('#life_insurance,#vehicle').hide();
   $('#policy_plan_attributes_company_categories_id').on('change', function(){
-    var selected = $('#policy_plan_attributes_company_categories_id :selected').text();
+    var selected_option = $('#policy_plan_attributes_company_categories_id :selected').text();
     var list = $.map($('#policy_plan_attributes_company_categories_id option'), function(ele) {
       return ele.value; 
     });
     remain = jQuery.grep(list, function(value) {
-      return value != selected;
+      return value != selected_option;
     });
-   if ($.inArray(selected, list) > -1)
-    {
-      selected_option = selected.replace(/ /g, "_").toLowerCase(); 
-      $('#'+selected_option).show();
-      var i;
-      for (i = 0; i < remain.length; i++) {
-        other_option=remain[i].replace(/ /g, "_").toLowerCase();
-        $('#'+other_option).hide();
-      }
+    if ($.inArray(selected_option, list) > -1)
+      {
+        selected_option = selected_option.replace(/ /g, "_").toLowerCase(); 
+        $('#'+selected_option).show();
+        var i;
+        for (i = 0; i < remain.length; i++) {
+          other_option=remain[i].replace(/ /g, "_").toLowerCase();
+          $('#'+other_option).hide();
+        }
     }
   });
 },
@@ -43,26 +43,24 @@ buttonOfForm: function(){
   $('.buttonNext').addClass('btn btn-success');
   $('.buttonPrevious').addClass('btn btn-primary');
   $('.buttonFinish').addClass('btn btn-default');
-  $('#policy_plan_attributes_company_categories_id').on('change', function(){
-    var selected = $('#policy_plan_attributes_company_categories_id :selected').text();
+  $('.buttonFinish').on('click', function(){
+    var selected_option = $('#policy_plan_attributes_company_categories_id :selected').text();
     var list = $.map($('#policy_plan_attributes_company_categories_id option'), function(ele) {
       return ele.value; 
     });
     remain = jQuery.grep(list, function(value) {
-      return value != selected;
+      return value != selected_option;
     });
-   if ($.inArray(selected, list) > -1)
+   if ($.inArray(selected_option, list) > -1)
     {
-      selected_option = selected.replace(/ /g, "_").toLowerCase(); 
-      $('#'+res).show();
+      selected_option = selected_option.replace(/ /g, "_").toLowerCase(); 
+      $('#'+selected_option).show();
       var i;
       for (i = 0; i < remain.length; i++) {
         other_option=remain[i].replace(/ /g, "_").toLowerCase();
-        $('#'+res1).remove();
+        $('#'+other_option).remove();
       }
     }
-  });
-  $('.buttonFinish').on('click', function(){
     $('form').submit();
   })
 },
