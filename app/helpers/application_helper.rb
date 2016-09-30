29d95a7aab9bcb7e26ccb801cc86937c65c000f8
AmiFaldu-BTC <ami.faldu@botreetechnologies.com>
2016-09-30 18:20:36 +0530

    #32 flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,19 @@ module ApplicationHelper
 	def void_link
     JAVASCRIPT_VOID
   end
+#flash type
+  def bootstrap_class(flash_type)
+    case flash_type
+    when "success"
+      "alert-success"
+    when "error"
+      "alert-danger"
+    when "alert"
+      "alert-warning"
+    when "notice"
+      "alert-info"
+    else
+      flash_type.to_s
+    end
+  end
 end
