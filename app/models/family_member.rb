5a7da911eb6ce41f90fd40852531d79e49ed4770
RaviMariya-BTC <ravi.mariya@botreetechnologies.com>
2016-10-03 18:53:07 +0530

    customer registration with family_member model #5

--- app/models/family_member.rb
+++ app/models/family_member.rb
@@ -1,7 +1,6 @@
 class FamilyMember < ApplicationRecord
-  belongs_to :user
-  has_one :family
-  has_many :personal_info, as: :informable, dependent: :destroy
+  has_one :user, through: :family
+  has_one :personal_info, as: :informable, dependent: :destroy
   accepts_nested_attributes_for :personal_info
   has_and_belongs_to_many :life_insurances
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,10 @@
 # Creating Categories with the default information.
 
 CATEGORIES = [{name: "Life Insurance"}, {name: "Vehicle"}, {name: "Medical and Health"}, {name: "Fire"}, {name: "Accident"}]
+RELATIONS = [{ relation_type: 'Brother' }, { relation_type: 'Mother' }, { relation_type: 'Father' }, { relation_type: 'Sister' }, { relation_type: 'Son' }, { relation_type: 'Daughter' }, { relation_type: 'Grand-Mother' }, { relation_type: 'Grand-Father' }, { relation_type: 'Father-In-Law' }, { relation_type: 'Mother-In-Law'}]
 
 puts "Start creating the required categories data for application."
 CATEGORIES.each{ |categories| Category.find_or_create_by!(categories)}
 puts "All the required categories data has been added successfully."
+
+RELATIONS.each{ |relations| Relation.find_or_create_by!(relations)}
