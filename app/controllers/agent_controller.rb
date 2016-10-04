62c423fdb79661cb7bafc7b22013064d4c957260
AmiFaldu-BTC <ami.faldu@botreetechnologies.com>
2016-10-04 15:42:53 +0530

    #47 dasboard company,policy,customer count display

--- app/controllers/agent_controller.rb
+++ app/controllers/agent_controller.rb
@@ -2,5 +2,8 @@ class AgentController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
+    @policy_count = Policy.count
+    @customer_count = User.count
+    @company_count = Company.count
   end
 end
