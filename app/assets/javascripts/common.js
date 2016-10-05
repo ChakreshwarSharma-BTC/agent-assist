07bb0ed24b7642b87a777670bede8dd27de22ba2
Ravi Mariya <ravi.mariya@botreetechnologies.com>
2016-10-05 18:01:46 +0530

    list customer and apply filters #11 (#60)

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -9,25 +9,28 @@ AgentAssist.Common = {
   Flash_message: function(){
     $(".flash_msg").delay(5000).slideUp(3000).fadeOut();
   },
-  Auto_load: function(){
+  applyLoader: function () {
+    $.blockUI({css:{
+      border: 'none',
+      padding: '15px',
+      backgroundColor: '#000',
+      '-webkit-border-radius': '10px',
+      '-moz-border-radius': '10px',
+      opacity: .5,
+      color: '#fff'
+    }});
+  },
+  ajaxLoader: function () {
     $(document).ajaxStart(function () {
-      $.blockUI({ css: {
-          border: 'none',
-          padding: '15px',
-          backgroundColor: '#000',
-          '-webkit-border-radius': '10px',
-          '-moz-border-radius': '10px',
-          opacity: .5,
-          color: '#fff'
-      } });
+      AgentAssist.Common.applyLoader();
     }).ajaxStop(function () {
-        $.unblockUI();
+      $.unblockUI();
     });
   },
   documentOnReady: function (){
     AgentAssist.Common.selectDropDown();
     AgentAssist.Common.Flash_message();
-    AgentAssist.Common.Auto_load();
+    AgentAssist.Common.ajaxLoader();
   }
 };
 $(document).ready(function(){
