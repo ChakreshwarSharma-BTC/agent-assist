b240ea2266d82d37a80607d7f6657f5b4b87b744
RaviMariya-BTC <ravi.mariya@botreetechnologies.com>
2016-10-17 12:00:46 +0530

    Removed buttons when disabled in smartwizard  #79

--- app/assets/javascripts/SmartWizard.js
+++ app/assets/javascripts/SmartWizard.js
@@ -441,7 +441,7 @@ $.fn.smartWizard.defaults = {
     contentCache:true, // cache step contents, if false content is fetched always from ajax url
     cycleSteps: false, // cycle step navigation
     enableFinishButton: false, // make finish button enabled always
-	hideButtonsOnDisabled: false, // when the previous/next/finish buttons are disabled, hide them instead?
+	hideButtonsOnDisabled: true, // when the previous/next/finish buttons are disabled, hide them instead?
     errorSteps:[],    // Array Steps with errors
     labelNext:'Next',
     labelPrevious:'Previous',
